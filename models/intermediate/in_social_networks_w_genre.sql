3818690ccb8cdd47f88df62fdddd226c11e35e2f
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-11-27 13:19:40 +0000

    New models

--- models/intermediate/in_social_networks_w_genre.sql
+++ models/intermediate/in_social_networks_w_genre.sql
@@ -1,6 +1,44 @@
+WITH sub AS (
+    SELECT
+        artist,
+        genre_name,
+        spotify_id
+    FROM {{ ref("stg_musi_graph_ready__music_genres_songs_clean") }}
+)
 SELECT
-    *
-FROM {{ ref("int_social_networks") }}` AS cfs
-LEFT JOIN {{ ref("stg_musi_graph_ready__music_genres_songs_clean") }} AS mg
+    track_id,
+        track_name,
+        album_id,
+        artist_ids,
+        artist_names,
+        valence,
+        year,
+        acousticness,
+        danceability,
+        duration_ms,
+        energy,
+        explicit,
+        instrumentalness,
+        key,
+        liveness,
+        loudness,
+        mode,
+        popularity,
+        speechiness,
+        tempo,
+        spotify_streams,
+        spotify_playlist_count,
+        spotify_playlist_reach,
+        spotify_popularity,
+        youtube_views,
+        youtube_likes,
+        tiktok_likes,
+        tiktok_views,
+        youtube_playlist_reach,
+        mg.artist,
+        mg.genre_name,
+        mg.spotify_id
+FROM {{ ref("int_social_networks") }} AS cfs
+LEFT JOIN sub AS mg
 ON cfs.artist = mg.artist
-WHERE genre_name IS NOT NULL AND genre_name <> ""
+WHERE genre_name IS NOT NULL AND genre_name != ""
--- models/staging/musi_graph_ready/stg_musi_graph_ready__spotify_most_streamed_2024.sql
+++ models/staging/musi_graph_ready/stg_musi_graph_ready__spotify_most_streamed_2024.sql
@@ -10,20 +10,20 @@ renamed as (
 
     select
         track,
-        "album name" AS album_name,
+        `album name` AS album_name,
         artist,
-        "release date" as release_date,
-        "all time rank" AS all_time_rank,
-        "track score" AS track_score,
-        "spotify streams" AS spotify_streams,
-        "spotify playlist count" AS spotify_playlist_count,
-        "spotify playlist reach" AS spotify_playlist_reach,
-        "spotify popularity" AS spotify_popularity,
-        "youtube views" AS youtube_views,
-        "youtube likes" AS youtube_likes,
-        "tiktok likes" AS tiktok_likes,
-        "tiktok views" AS tiktok_views,
-        "youtube playlist reach" AS youtube_playlist_reach
+        `release date` as release_date,
+        `all time rank` AS all_time_rank,
+        `track score` AS track_score,
+        `spotify streams` AS spotify_streams,
+        `spotify playlist count` AS spotify_playlist_count,
+        `spotify playlist reach` AS spotify_playlist_reach,
+        `spotify popularity` AS spotify_popularity,
+        `youtube views` AS youtube_views,
+        `youtube likes` AS youtube_likes,
+        `tiktok likes` AS tiktok_likes,
+        `tiktok views` AS tiktok_views,
+        `youtube playlist reach` AS youtube_playlist_reach
 
     from source
 
