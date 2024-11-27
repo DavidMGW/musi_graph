90a450832e4e82bef3dbdb21fbd81e93e45736e9
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-11-27 10:49:59 +0000

    Added int_social_networks

--- models/intermediate/int_social_networks.sql
+++ models/intermediate/int_social_networks.sql
@@ -1,4 +1,17 @@
-SELECT *
-    ,CONCAT(TRIM(LOWER(track)), "_", TRIM(LOWER(artist))) AS pk 
-FROM {{ ref("stg_musi_graph_ready__spotify_most_streamed_2024") }}
-LIMIT 50
+WITH sub AS (
+    SELECT
+        *,
+        REPLACE(REPLACE(REPLACE(CONCAT(TRIM(LOWER(artist_names)), '_', TRIM(LOWER(track_name))), '[', ''), ']', ''), "'", '') AS PK
+    FROM {{ref("stg_musi_graph_ready__spotify_songs_features")}}
+),
+sub2 AS (
+    SELECT
+        *,
+        CONCAT(LOWER(Artist), '_', (LOWER(Track))) AS PK
+    FROM {{ref("stg_musi_graph_ready__spotify_most_streamed_2024") }}
+)
+SELECT
+    *,
+FROM sub
+RIGHT JOIN sub2 USING (PK)
+WHERE track_id IS NOT NULL
