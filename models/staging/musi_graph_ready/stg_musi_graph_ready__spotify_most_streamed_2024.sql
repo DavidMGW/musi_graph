2b9579b8dc41c7e743bf89466af9eeb4e7bc1247
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-11-27 10:07:51 +0000

    Fixed column names problems

--- models/staging/musi_graph_ready/stg_musi_graph_ready__spotify_most_streamed_2024.sql
+++ models/staging/musi_graph_ready/stg_musi_graph_ready__spotify_most_streamed_2024.sql
@@ -10,20 +10,20 @@ renamed as (
 
     select
         track,
-        album name,
+        "album name" AS album_name,
         artist,
-        release date,
-        all time rank,
-        track score,
-        spotify streams,
-        spotify playlist count,
-        spotify playlist reach,
-        spotify popularity,
-        youtube views,
-        youtube likes,
-        tiktok likes,
-        tiktok views,
-        youtube playlist reach
+        "release date" as release_date,
+        "all time rank" AS all_time_rank,
+        "track score" AS track_score,
+        "spotify streams" AS spotify_streams,
+        "spotify playlist count" AS spotify_playlist_count,
+        "spotify playlist reach" AS spotify_playlist_reach,
+        "spotify popularity" AS spotify_popularity,
+        "youtube views" AS youtube_views,
+        "youtube likes" AS youtube_likes,
+        "tiktok likes" AS tiktok_likes,
+        "tiktok views" AS tiktok_views,
+        "youtube playlist reach" AS youtube_playlist_reach
 
     from source
 
