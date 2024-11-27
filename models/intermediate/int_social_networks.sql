c358a4c9db383c04cb5642851b739d741514bae3
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-11-27 10:56:15 +0000

    Finalize and build int_social_networks

--- models/intermediate/int_social_networks.sql
+++ models/intermediate/int_social_networks.sql
@@ -1,6 +1,25 @@
 WITH sub AS (
     SELECT
-        *,
+        track_id,
+        track_name,
+        album_id,
+        artist_ids,
+        artist_names,
+        valence,
+        year,
+        acousticness,
+        danceability,
+        duration_ms,
+        energy,
+        explicit,
+        instrumentalness,
+        key,
+        liveness,
+        loudness,
+        mode,
+        popularity,
+        speechiness,
+        tempo,
         REPLACE(REPLACE(REPLACE(CONCAT(TRIM(LOWER(artist_names)), '_', TRIM(LOWER(track_name))), '[', ''), ']', ''), "'", '') AS PK
     FROM {{ref("stg_musi_graph_ready__spotify_songs_features")}}
 ),
